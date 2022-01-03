b39062004171ed09ab44aa75fc86065c905f840b
Timo Kankaanpää <tka@puv.fi>
2022-01-03 20:00:12 +0200

    Template

---
+++ src/App.test.js
@@ -0,0 +1,9 @@
+import React from 'react';  //for gitlab CI test this need to be there even React 17 does not need it
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
